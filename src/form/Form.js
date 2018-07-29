a81322eec69b0fe678d804f4ca26bb9257abe34c
Ma7eer <maher.alkendi2@gmail.com>
2018-07-29 13:15:09 -0600

    added simple error handling

--- src/form/Form.js
+++ src/form/Form.js
@@ -3,7 +3,10 @@ import Axios from 'axios';
 
 export default class Form extends Component {
   state = {
-    pokemonName: ''
+    pokemonName: '',
+    formClasses: "form-control col-12",
+    errorMessage: "Opps! Please spell pokemon name correctly!"
+    // "Please enter a Pokemon's name"
   }
   handleSubmit = (event) => {
     event.preventDefault();
@@ -14,19 +17,37 @@ export default class Form extends Component {
       .then(res => {
         console.log(res.data);
         this.props.onSubmit(res.data);
-        this.setState({pokemonName: ''});
+        this.setState({pokemonName: '', formClasses: "form-control col-12"});
+      }).catch(error => {
+        if(error.request) {
+          if(this.state.pokemonName === '') {
+            this.setState(prevState => ({
+              formClasses: "is-invalid " + prevState.formClasses,
+              errorMessage: "Please enter a Pokemon's name!"
+            }))
+            console.log(error);
+          } else {
+            this.setState(prevState => ({
+              formClasses: "is-invalid " + prevState.formClasses
+            }))
+            console.log(error);
+          }
+        }
       });
     }
 
   render () {
     return (
-      <form onSubmit={this.handleSubmit} className="d-flex align-items-center px-5">
+      <form onSubmit={this.handleSubmit} className="d-flex align-items-center px-5" noValidate>
         <div className="input-group row pt-4 pb-3 mt-5 my-row align-items-center">
-            <input className="form-control col-12" type="text" placeholder="Pokemon" value={this.state.pokemonName}
+            <input className={this.state.formClasses} type="text" placeholder="Pokemon name or ID" value={this.state.pokemonName}
             onChange={(event) => this.setState({ pokemonName: event.target.value})} required/>
-          <div className="input-group-append">
+          <div className={"input-group-append"}>
             <button className="form-control btn btn-danger" type="submit">Submit</button>
           </div>
+          <div className="invalid-feedback" style={{fontSize:'20px'}}>
+              <strong>{this.state.errorMessage}</strong>
+            </div>
           </div>
       </form>
     )
