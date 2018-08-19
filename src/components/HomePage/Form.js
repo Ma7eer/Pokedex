b0f3aca9fb8f697a0056fb468e15e631e7dd51e7
Ma7eer <maher.alkendi2@gmail.com>
2018-08-19 08:56:03 -0600

    added typechecking + toastr for better error handleing

--- src/components/HomePage/Form.js
+++ src/components/HomePage/Form.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import PropTypes from 'prop-types';
+import toastr from 'toastr';
 
 export default class Form extends Component {
   state = {
-    pokemonName: '',
-    formClasses: "form-control col-12",
-    errorMessage: "Opps! Please spell pokemon name correctly!"
+    pokemonName: ''
   }
   handleSubmit = (event) => {
     event.preventDefault();
@@ -14,21 +14,16 @@ export default class Form extends Component {
 
     Axios.get(url)
       .then(res => {
-        console.log(res.data);
+        // console.log(res.data);
         this.props.onSubmit(res.data);
-        this.setState({pokemonName: '', formClasses: "form-control col-12"});
+        toastr.success("Yay! new Pokemon");
+        this.setState({pokemonName: ''});
       }).catch(error => {
         if(error.request && this.state.pokemonName === '') {
-            this.setState(prevState => ({
-              formClasses: "is-invalid " + prevState.formClasses,
-              errorMessage: "Please enter a Pokemon's name!"
-            }))
+          toastr.warning("Please enter a Pokemon's name!");
             console.log(error);
           } else {
-            this.setState(prevState => ({
-              formClasses: "is-invalid " + prevState.formClasses,
-              errorMessage: "Opps! Please spell pokemon name correctly!"
-            }))
+            toastr.error("Opps! Please spell pokemon name correctly!");
             console.log(error);
           }
         }
@@ -39,16 +34,17 @@ export default class Form extends Component {
     return (
       <form onSubmit={this.handleSubmit} className="d-flex align-items-center px-5" noValidate>
         <div className="input-group row pt-4 pb-3 mt-5 my-row align-items-center">
-            <input className={this.state.formClasses} type="text" placeholder="Pokemon name or ID" value={this.state.pokemonName}
+            <input className="form-control col-12" type="text" placeholder="Pokemon name or ID" value={this.state.pokemonName}
             onChange={(event) => this.setState({ pokemonName: event.target.value})} required/>
           <div className={"input-group-append"}>
             <button className="form-control btn btn-danger" type="submit">Submit</button>
           </div>
-          <div className="invalid-feedback" style={{fontSize:'20px'}}>
-              <strong>{this.state.errorMessage}</strong>
-            </div>
-          </div>
+        </div>
       </form>
     )
   }
+}
+
+Form.propTypes = {
+  onSubmit: PropTypes.func
 }
